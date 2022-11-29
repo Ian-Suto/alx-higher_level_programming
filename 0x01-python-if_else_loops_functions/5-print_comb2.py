a943dba1012cb0fc6a550ddd8998d4a90f3665bd
Ian Musima <sutomusima@gmail.com>
2022-11-29 19:28:43 +0000

    Correct checker error

--- 0x01-python-if_else_loops_functions/5-print_comb2.py
+++ 0x01-python-if_else_loops_functions/5-print_comb2.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 for i in range(100):
     if i == 0:
-        print(f"{i:0>2}", end="")
+        print("{:02d}".format(i), end="")
     else:
-        print(f", {i:0>2}", end="")
+        print(", {:02d}".format(i), end="")
