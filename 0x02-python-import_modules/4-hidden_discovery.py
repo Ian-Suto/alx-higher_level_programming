309dcd51779bcc40ba886edee867e01e901f20f3
Ian Musima <sutomusima@gmail.com>
2022-12-02 11:14:12 +0000

    Print definition in module

--- 0x02-python-import_modules/4-hidden_discovery.py
+++ 0x02-python-import_modules/4-hidden_discovery.py
@@ -1,3 +1,8 @@
 #!/usr/bin/python3
 if __name__ == "__main__":
-
+    import hidden_4
+    attribute_list = dir(hidden_4)
+    for attribute in attribute_list:
+        if attribute[:2] == "__":
+            continue
+        print("{}".format(attribute))
