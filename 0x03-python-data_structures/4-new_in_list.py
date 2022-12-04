45144860bf0dd617d3facd97a4c615cfbfe46a07
Ian Musima <sutomusima@gmail.com>
2022-12-04 11:17:55 +0000

    Correct error, third attempt

--- 0x03-python-data_structures/4-new_in_list.py
+++ 0x03-python-data_structures/4-new_in_list.py
@@ -1,8 +1,9 @@
 #!/usr/bin/python3
 def new_in_list(my_list, idx, element):
     copy_list = []
-    for i in range((len(my_list))):
-        copy_list.append(my_list[i])
-    if idx > 0 or idx < len(my_list) - 1:
-        copy_list[idx] = element
-    return copy_list
+    if my_list:
+        for i in range((len(my_list))):
+            copy_list.append(my_list[i])
+        if idx > 0 or idx < len(my_list) - 1:
+            copy_list[idx] = element
+        return copy_list
