da19ee598c3e6fe7cd7aa1d103fab2bcef617075
Ian Musima <sutomusima@gmail.com>
2022-12-06 20:08:05 +0000

    Correct checker error

--- 0x03-python-data_structures/8-multiple_returns.py
+++ 0x03-python-data_structures/8-multiple_returns.py
@@ -1,9 +1,9 @@
 #!/usr/bin/python3
 def multiple_returns(sentence):
     length = len(sentence)
-    if sentence:
-        first = sentence[0]
+    if sentence == '':
+        first = None
         return length, first
     else:
-        first = None
-        return lenght, first
+        first = sentence[0]
+        return length, first
