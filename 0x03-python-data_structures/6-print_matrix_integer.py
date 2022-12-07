ccaaed896a81b1a2c55a700d1c22a1735204ccd3
Ian Musima <sutomusima@gmail.com>
2022-12-07 13:07:41 +0000

    Print matrix

--- 0x03-python-data_structures/6-print_matrix_integer.py
+++ 0x03-python-data_structures/6-print_matrix_integer.py
@@ -2,5 +2,8 @@
 def print_matrix_integer(matrix=[[]]):
     for i in matrix:
         for j in i:
-            print("{}".format(j), end="  ")
+            if j == i[-1]:
+                print("{}".format(j), end="")
+            else:
+                print("{}".format(j), end="  ")
         print()
