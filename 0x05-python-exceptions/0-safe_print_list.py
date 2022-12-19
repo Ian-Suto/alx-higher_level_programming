2efe9e8aeaaa83d00ad069681a537e1140936147
Ian Musima <sutomusima@gmail.com>
2022-12-19 20:03:22 +0000

    Handle index exception

--- 0x05-python-exceptions/0-safe_print_list.py
+++ 0x05-python-exceptions/0-safe_print_list.py
@@ -1,8 +1,11 @@
 #!/usr/bin/python3
 def safe_print_list(my_list=[], x=0):
     j = 0
-    i = 0
-    list=[]
-    
+    for i in range(x):
+        try:
+            print("{}".format(my_list[i]), end="")
+            j += 1
+        except IndexError:
+            pass
+    print("")
     return (j)
-
