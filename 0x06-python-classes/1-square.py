4b7896ef470276b8e2e2c1702b6346e0cd3437ac
Ian Musima <sutomusima@gmail.com>
2022-12-21 07:20:47 +0000

    Create private attribute

---
+++ 0x06-python-classes/1-square.py
@@ -0,0 +1,9 @@
+#!/usr/bin/python3
+"""Module for calculating a number's square"""
+
+
+class Square:
+    """A class for finding the square of a number"""
+    def __init__(self, size):
+        """A method square"""
+        self.__size = size
