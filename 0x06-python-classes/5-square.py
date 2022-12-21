7d40bc5a44fe3c2f8e5cb3371d61ea701202a4e8
Ian Musima <sutomusima@gmail.com>
2022-12-21 15:46:58 +0000

    Create the nethod my_print

---
+++ 0x06-python-classes/5-square.py
@@ -0,0 +1,38 @@
+#!/usr/bin/python3
+"""Module for calculating a number's square"""
+
+
+class Square:
+    """A class for finding the square of a number"""
+    def __init__(self, size=0):
+        """A method square"""
+        self.size = size
+
+    @property
+    def size(self):
+        """Getter method for the attribute size"""
+        return self.__size
+
+    @size.setter
+    def size(self, value):
+        """Setter method for the attribute size"""
+        if type(value) != int:
+            raise TypeError("size must be an integer")
+        elif value < 0:
+            raise ValueError("size must be >= 0")
+        else:
+            self.__size = value
+
+    def area(self):
+        """Returns the current square area"""
+        return (self.__size**2)
+
+    def my_print(self):
+        """Print in stdout the square with the character #"""
+        if self.__size == 0:
+            print('')
+        else:
+            for i in range(self.__size):
+                for i in range(self.__size):
+                    print("#", end="")
+                print('')
