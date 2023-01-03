3adedab7f0583f97213a3de3cd1ec9936e55d335
Ian Musima <sutomusima@gmail.com>
2023-01-03 14:12:49 +0000

    Create empty class

---
+++ 0x08-python-more_classes/0-rectangle.py
@@ -0,0 +1,7 @@
+#!/usr/bin/python3
+"""An empty class representing a rectangle"""
+
+
+class Rectangle:
+    """An example of a class, representing a rectangle"""
+    pass
---
+++ 0x08-python-more_classes/README.md
@@ -0,0 +1,1 @@
+Tutorilas on classes
