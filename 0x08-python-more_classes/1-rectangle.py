434888f5136869eb163913ac11335a93dc9dfd26
Ian Musima <sutomusima@gmail.com>
2023-01-03 14:56:25 +0000

    Create private attributes width and height

---
+++ 0x08-python-more_classes/1-rectangle.py
@@ -0,0 +1,40 @@
+#!/usr/bin/python3
+"""An empty class representing a rectangle"""
+
+
+class Rectangle:
+    """An example of a class, representing a rectangle"""
+    def __init__(self, width=0, height=0):
+        """Initialize width attribute"""
+        self.__width = width
+        self.__height = height
+
+    @property
+    def width(self):
+        """Getter method for width attribute"""
+        return self.__width
+
+    @width.setter
+    def width(self, value):
+        """Setter method for width attribute"""
+        if type(value) != int:
+            raise TypeError("width must be an integer")
+        elif value < 0:
+            raise ValueError("width must be >= 0")
+        else:
+            self.__width = value
+
+    @property
+    def height(self):
+        """Getter method for height attribute"""
+        return self.__height
+
+    @height.setter
+    def height(self, value):
+        """Setter method for height attribute"""
+        if type(value) != int:
+            raise TypeError("height must be an integer")
+        elif value < 0:
+            raise ValueError("height must be >= 0")
+        else:
+            self.__height = value
