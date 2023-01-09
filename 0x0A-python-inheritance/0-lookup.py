244529f2e034400f5e2280927f81e63551bead65
Ian Musima <sutomusima@gmail.com>
2023-01-09 20:29:01 +0000

    Correct error

--- 0x0A-python-inheritance/0-lookup.py
+++ 0x0A-python-inheritance/0-lookup.py
@@ -1,4 +1,7 @@
 #!/usr/bin/python3
+"""Defines a method lookup"""
+
+
 def lookup(obj):
     """Return a list of all methods and attributes of the given object"""
-    return (list(dir(obj)))
+    return (dir(obj))
