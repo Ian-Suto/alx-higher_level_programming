351dc1dcec1aa8dd2cbd3d8a4ee466fe646c3674
Ian Musima <sutomusima@gmail.com>
2023-01-25 14:41:43 +0000

    Generate Pascal triangle

---
+++ 0x0B-python-input_output/11-student.py
@@ -0,0 +1,22 @@
+#!/usr/bin/python3
+"""Define a class Student"""
+
+
+class Student:
+    """Defining a student profile"""
+    def __init__(self, first_name, last_name, age):
+        """Initialize Student attributes"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.age = age
+
+    def to_json(self, attrs=None):
+        """Retrieves a dictionary representation of a Student instance"""
+        dict_rep = self.__dict__
+        dicti = {}
+        if type(attrs) is list:
+            for i in attrs:
+                if i in dict_rep:
+                    dicti.update({i: dict_rep[i]})
+            return dicti
+        return (dict_rep)
---
+++ 0x0B-python-input_output/12-pascal_triangle.py
@@ -0,0 +1,15 @@
+#!/usr/bin/python3
+"""Module generate's a Pascal triangle"""
+
+
+def pascal_triangle(n):
+    """Return's a list of lists of int rep the Pascal triangle of n."""
+    result = [[1]]
+
+    for i in range(n - 1):
+        tmp = [0] + result[-1] + [0]
+        row = []
+        for j in range(len(result[-1]) + 1):
+            row.append(tmp[j] + tmp[j + 1])
+        result.append(row)
+    return result
