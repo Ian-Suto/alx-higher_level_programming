998720a41022c1330112b9b6d1e1f48d05015f5f
Ian Musima <sutomusima@gmail.com>
2023-02-15 21:05:14 +0000

    Fix checker

--- 0x0E-SQL_more_queries/1-create_user.sql
+++ 0x0E-SQL_more_queries/1-create_user.sql
@@ -1,3 +1,4 @@
 -- Create user and grant privileges
 CREATE USER 'user_0d_1'@'localhost' IDENTIFIED BY 'user_0d_1_pwd';
-GRANT ALL PRIVILEGES ON *.* TO 'user_od_1'@'localhost' WITH GRANT OPTION;
+GRANT ALL PRIVILEGES ON *.* TO 'user_od_1'@'localhost';
+FLUSH PRIVILEGES;
