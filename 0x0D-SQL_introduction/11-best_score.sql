46cd015440f36909acb740b494dcc027e247fc7b
Ian Musima <sutomusima@gmail.com>
2023-02-16 15:07:22 +0000

    Filter and remove records

--- 0x0D-SQL_introduction/11-best_score.sql
+++ 0x0D-SQL_introduction/11-best_score.sql
@@ -1,3 +1,2 @@
 -- List all records with a score >= 10 in descending order
--- SELECT score, name FROM second_table ORDER BY score >= 10 DESC;
 SELECT score, name FROM second_table WHERE score >= 10 ORDER BY score DESC;
