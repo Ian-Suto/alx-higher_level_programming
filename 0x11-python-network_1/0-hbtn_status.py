eb00ea6bdf56b46487ab811e1e8a0163db1d8b26
Ian Musima <sutomusima@gmail.com>
2023-03-31 13:19:10 +0000

    Fix pycodestyle error

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -8,5 +8,5 @@ if __name__ == "__main__":
         response = res_msg.read()
         print("Body reponse:")
         print("\t- type: {}".format(type(response)))
-		print(f"\t- content: {response}")
+        print(f"\t- content: {response}")
         print("\t- utf8 content: {}".format(response.decode("utf-8")))
