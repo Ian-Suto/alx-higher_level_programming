4dc65b760e75882c509033746f6aa4bd7a946359
Ian Musima <sutomusima@gmail.com>
2023-03-31 13:22:03 +0000

    Add spacing

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -2,6 +2,7 @@
 """Fetch https://intranet.hbtn.io/status specifying output."""
 import urllib.request
 
+
 if __name__ == "__main__":
     url = 'https://alx-intranet.hbtn.io/status'
     with urllib.request.urlopen(url) as res_msg:
