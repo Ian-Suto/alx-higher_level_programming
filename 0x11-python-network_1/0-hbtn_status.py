ef4e39e77c8f01b62afbefc41fb29c453fe4d1f6
Ian Musima <sutomusima@gmail.com>
2023-03-31 13:34:49 +0000

    Check if response is readable

--- 0x11-python-network_1/0-hbtn_status.py
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -6,8 +6,9 @@ import urllib.request
 if __name__ == "__main__":
     url = 'https://alx-intranet.hbtn.io/status'
     with urllib.request.urlopen(url) as res_msg:
-        response = res_msg.read()
-        print("Body reponse:")
-        print("\t- type: {}".format(type(response)))
-        print("\t- content: {}".format(response))
-        print("\t- utf8 content: {}".format(response.decode("utf-8")))
+        if res_msg.readable():
+            response = res_msg.read()
+            print("Body reponse:")
+            print("\t- type: {}".format(type(response)))
+            print("\t- content: {}".format(response))
+            print("\t- utf8 content: {}".format(response.decode("utf-8")))
