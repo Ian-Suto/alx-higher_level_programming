f4c74d98846fa864843c093b337692bd615fcb49
Ian Musima <sutomusima@gmail.com>
2023-03-31 13:16:40 +0000

    Fetch url

---
+++ 0x11-python-network_1/0-hbtn_status.py
@@ -0,0 +1,12 @@
+#!/usr/bin/python3
+"""Fetch https://intranet.hbtn.io/status specifying output."""
+import urllib.request
+
+if __name__ == "__main__":
+    url = 'https://alx-intranet.hbtn.io/status'
+    with urllib.request.urlopen(url) as res_msg:
+        response = res_msg.read()
+        print("Body reponse:")
+        print("\t- type: {}".format(type(response)))
+		print(f"\t- content: {response}")
+        print("\t- utf8 content: {}".format(response.decode("utf-8")))
---
+++ 0x11-python-network_1/README.md
@@ -0,0 +1,1 @@
+Tutorilas on Networks
