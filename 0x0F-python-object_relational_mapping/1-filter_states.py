857630c29fb8d544911354b6178cca8fb55fbe98
Ian Musima <sutomusima@gmail.com>
2023-04-19 06:22:02 -0700

    Fix pycodestyle error

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -7,7 +7,8 @@ if __name__ == '__main__':
     connect = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                               passwd=sys.argv[2], db=sys.argv[3])
     cur = connect.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY 'N%' ORDER BY id ASC")
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY
+                'N%' ORDER BY id ASC")
     us_states = cur.fetchall()
     for us_state in us_states:
         print(us_state)
