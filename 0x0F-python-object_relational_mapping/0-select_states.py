6aa800b92edc4faf64728d1ab794c8f2bf3f2f26
Ian Musima <sutomusima@gmail.com>
2023-04-19 05:32:12 -0700

    lists all states from the database hbtn_0e_0_usa

---
+++ 0x0F-python-object_relational_mapping/0-select_states.py
@@ -0,0 +1,13 @@
+#!/usr/bin/python3
+"""Script that lists all states from the database hbtn_0e_0_usa"""
+import sys
+import MySQLdb
+
+if __name__ == '__main__':
+    connect = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
+                              passwd=sys.argv[2], db=sys.argv[3])
+    cur = connect.cursor()
+    cur.execute("SELECT * FROM states ORDER BY id ASC")
+    us_states = cur.fetchall()
+    for us_state in us_states:
+        print(us_state)
---
+++ 0x0F-python-object_relational_mapping/README.md
@@ -0,0 +1,1 @@
+Tutorials on object relational mapping
