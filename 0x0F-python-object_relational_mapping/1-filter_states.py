1e3117d8d1ca41556e1270da5390ad69992e67df
Ian Musima <sutomusima@gmail.com>
2023-04-19 06:40:12 -0700

    Fix EOL error

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,5 +1,5 @@
 #!/usr/bin/python3
-"""lists all states that start with N """
+""" lists all states that start with N """
 import sys
 import MySQLdb
 
@@ -7,7 +7,7 @@ if __name__ == '__main__':
     connect = MySQLdb.connect(host="localhost", port=3306, user=sys.argv[1],
                               passwd=sys.argv[2], db=sys.argv[3])
     cur = connect.cursor()
-    cur.execute("SELECT * FROM states WHERE name LIKE BINARY
+    cur.execute("SELECT * FROM states WHERE name LIKE BINARY\
                 'N%' ORDER BY id ASC")
     us_states = cur.fetchall()
     for us_state in us_states:
