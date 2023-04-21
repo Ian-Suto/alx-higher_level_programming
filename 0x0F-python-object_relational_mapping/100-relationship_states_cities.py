b3243bcbc9ce8eb69206132d2fadef9d5a0ca2ac
Ian Musima <sutomusima@gmail.com>
2023-04-21 13:58:12 -0700

    Add advanced task 100

---
+++ 0x0F-python-object_relational_mapping/100-relationship_states_cities.py
@@ -0,0 +1,25 @@
+#!/usr/bin/python3
+""" Prints all City objects from the database
+"""
+import sys
+from relationship_state import Base, State
+from sqlalchemy import create_engine, asc
+from sqlalchemy.orm import sessionmaker
+from relationship_city import City
+from sqlalchemy.schema import Table
+
+if __name__ == "__main__":
+    engine = create_engine('mysql+mysqldb://{}:{}@localhost/{}'
+                           .format(sys.argv[1], sys.argv[2], sys.argv[3]),
+                           pool_pre_ping=True)
+    Base.metadata.create_all(engine)
+    Session = sessionmaker(bind=engine)
+    session = Session()
+
+    new_state = State(name='California')
+    session.add(new_state)
+    session.commit()
+
+    new_city = City(name='San Francisco', state=new_state)
+    session.add(new_city)
+    session.commit()
