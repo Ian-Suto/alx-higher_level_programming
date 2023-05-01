f5e8f37e774235cbdcea95524576b0846a68de07
Ian Musima <sutomusima@gmail.com>
2023-05-01 08:06:16 -0700

    Add corrected task 2

--- 0x11-python-network_1/2-post_email.py
+++ 0x11-python-network_1/2-post_email.py
@@ -1,5 +1,6 @@
 #!/usr/bin/python3
-"""Script  takes in a URL and an email, sends a POST request and displays response"""
+"""Script  takes in a URL and an email,
+sends a POST request and displays response"""
 import urllib.parse
 import urllib.request
 import sys
@@ -7,9 +8,9 @@ import sys
 
 if __name__ == "__main__":
     url = sys.argv[1]
-    values = {'email':sys.argv[2]}
+    values = {'email': sys.argv[2]}
 
-    data = urllib.parse.urlencode(email)
+    data = urllib.parse.urlencode(values)
     data = data.encode('ascii')
     req = urllib.request.Request(url, data)
     with urllib.request.urlopen(req) as response:
