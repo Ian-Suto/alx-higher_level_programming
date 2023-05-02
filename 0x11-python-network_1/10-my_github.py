45ac3327d4a078177b7bae7a3cd201ef1751fa76
Ian Musima <sutomusima@gmail.com>
2023-05-02 10:28:22 -0700

    Add task 10

---
+++ 0x11-python-network_1/10-my_github.py
@@ -0,0 +1,14 @@
+#!/usr/bin/python3
+"""Script that takes GitHub credentials
+and uses the GitHub API to display the user id
+"""
+from requests import get, auth
+import sys
+
+
+if __name__ == "__main__":
+    user = sys.argv[1]
+    password = sys.argv[2]
+    url = 'https://api.github.com/user'
+    r = get(url, auth=auth.HTTPBasicAuth(user, password))
+    print(r.json().get('id'))
