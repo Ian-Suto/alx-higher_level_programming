34a9b998f142ce8eb449d802930995d38ad243a1
Ian Musima <sutomusima@gmail.com>
2023-05-02 10:47:46 -0700

    Fix checker error

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -2,13 +2,19 @@
 """Script that takes GitHub credentials
 and uses the GitHub API to display the user id
 """
-from requests import get, auth
+from requests import get
 import sys
 
 
 if __name__ == "__main__":
     user = sys.argv[1]
     password = sys.argv[2]
-    url = 'https://api.github.com/user'
-    r = get(url, auth=auth.HTTPBasicAuth(user, password))
-    print(r.json().get('id'))
+    url = 'https://api.github.com/users/' + user
+    headers = {'Accept': 'application/vnd.github+json',
+               'Authorization': 'Bearer ' + passwd}
+    response = get(url, headers=headers)
+    user_info = json.loads(response.text)
+    try:
+        print(user['id'])
+    except Exception as ex:
+        return
